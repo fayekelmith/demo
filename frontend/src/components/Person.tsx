31e7de02c63912062cf6ae0d56b0cd34fd0a4c1b
kelmith <faye.12222883@lpu.in>
2025-05-17 04:58:08 +0530

    update 3

--- frontend/src/components/Person.tsx
+++ frontend/src/components/Person.tsx
@@ -3,3 +3,12 @@ export interface Person {
   name: string;
   email: string;
 }
+
+export const PersonComponent = ({ person }: { person: Person }) => {
+  return (
+    <div>
+      <h3>{person.name}</h3>
+      <p>{person.email}</p>
+    </div>
+  );
+};
