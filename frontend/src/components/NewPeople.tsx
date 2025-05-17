03daba0da53b37e24948e881be314a27c80ae7d6
kelmith <faye.12222883@lpu.in>
2025-05-17 15:59:05 +0530

    update 11

--- frontend/src/components/NewPeople.tsx
+++ frontend/src/components/NewPeople.tsx
@@ -6,6 +6,7 @@ let name = "NewPeople";
 let name2 = "People";
 let name3 = "Person";
 let name4 = "api";
+let name5 = "useEffect";
 
 export default function NewPeople() {
   const [people, setPeople] = useState<Person[]>([]);
