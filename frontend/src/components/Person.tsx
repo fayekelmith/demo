6fe5d52f1882e21d94e3090d3d0f1c5cab6c124f
kelmith <faye.12222883@lpu.in>
2025-05-17 13:27:35 +0530

    update 6

--- frontend/src/components/Person.tsx
+++ frontend/src/components/Person.tsx
@@ -3,3 +3,25 @@ export interface Person {
   name: string;
   email: string;
 }
+
+export class Person {
+  id: number;
+  name: string;
+  email: string;
+
+  constructor(id: number, name: string, email: string) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+  static fromJson(json: any): Person {
+    return new Person(json.id, json.name, json.email);
+  }
+  toJson(): any {
+    return {
+      id: this.id,
+      name: this.name,
+      email: this.email,
+    };
+  }
+}
