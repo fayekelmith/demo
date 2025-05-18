e771452c30ca6362f37969d8aa622bcd33a00a3d
kelmith <faye.12222883@lpu.in>
2025-05-18 09:56:32 +0530

    update 19

---
+++ frontend/src/components/GraphDemo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface GraphDemoProps {
+  // Add props here if needed
+}
+
+const GraphDemo: React.FC<GraphDemoProps> = () => {
+  return (
+    <div className="graph-demo">
+      <h2>Graph Demo</h2>
+      {/* Add your graph components here */}
+    </div>
+  );
+};
+
+export default GraphDemo;
